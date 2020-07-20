804174fa68d34a5dccf5f7bf9042add450d50cf6
Renato Henrique <renato.henrique_98@hotmail.com>
2020-07-19 21:39:15 -0300

    Finalizando video #3 https://youtu.be/HQbzO0xDuX8

--- src/core/Engine.ts
+++ src/core/Engine.ts
@@ -1,30 +1,56 @@
 import WebGL from "./gl/WebGL";
+import Shader from "./gl/Shader";
 
 export default class Engine {
+  private _canvas: HTMLCanvasElement;
+  private _shader: Shader;
+
+  constructor() {}
+
+  public start(): void {
+    this._canvas = WebGL.initialize();
+
+    WebGL.context.clearColor(0, 0, 0, 1);
+
+    this.leadShaders();
+    this._shader.use();
+
+    this.loop();
+  }
+
+  public resize(): void {
+    if (this._canvas) {
+      this._canvas.width = window.innerWidth;
+      this._canvas.height = window.innerHeight;
+    }
+  }
+
+  private loop(): void {
+    WebGL.context.clear(WebGL.context.COLOR_BUFFER_BIT);
+    requestAnimationFrame(this.loop.bind(this));
+  }
+
+  private leadShaders(): void {
+    let vertexSource = `
+    attribute vec3 a_position;
     
-    private canvas: HTMLCanvasElement;
-
-    constructor() { }
-
-    public start(): void {
-
-        this.canvas = WebGL.initialize();
-
-        WebGL.context.clearColor(0, 0, 0, 1);
-
-        this.loop();
+    void main() {
+        gl_Position = vec4(a_position, 1.0);
     }
+    `;
 
-    public resize(): void {
-        if (this.canvas) {
-            this.canvas.width = window.innerWidth;
-            this.canvas.height = window.innerHeight;
-        }
+    let fragmentSource = `
+    precision mediump float;
+
+    void main() {
+        gl_FragColor = vec4(1.0);
     }
+    `;
 
-    private loop(): void {
-        WebGL.context.clear(WebGL.context.COLOR_BUFFER_BIT);
-
-        requestAnimationFrame(this.loop.bind(this));
-    }
+    this._shader = new Shader({
+      name: "basic",
+      vertexSource,
+      fragmentSource,
+    });
+  }
 }
